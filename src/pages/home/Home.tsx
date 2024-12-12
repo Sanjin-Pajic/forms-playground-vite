f6d5be1ea4e0bab081cdb8df2ece316197607dcd
Sanjin Pajic <pajicsanjo@gmail.com>
2024-12-12 20:12:02 +0100

    refactor: refactor a few files, add default Home route, convert eslint.config.js -> eslint.config.ts

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,21 @@
+/**
+ * HOME COMPONENT
+ * 
+ * This component serves as the primary entry point for the application (excluding index.tsx or App.tsx).
+ * 
+ * The App.tsx file is designed to include only this component as its main content. 
+ * While the original design does not involve routing or multiple pages, this setup 
+ * separates the primary UI logic from the App.tsx file, adhering to cleaner architectural 
+ * principles (in my opinion). By doing this, we ensure that the main application component,
+ * where optimally only routing logic and global components should be held, is not cluttered 
+ * with direct TSX content and that future scalability is preserved if additional routes or 
+ * components are to be added later (if this was an actual production-based project).
+ * 
+ * @function Home
+ * @returns {JSX.Element}
+ */
+function Home(): JSX.Element {
+    return <h1>Home.tsx</h1>;
+}
+
+export default Home;

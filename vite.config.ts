c2c8ca61f8d73075d95483ec35c83946319321b5
Sanjin Pajic <pajicsanjo@gmail.com>
2024-12-12 23:21:28 +0100

    feat: add SVG handling & rendering logic
    - Installed & configured SVGR to use SVG files from dedicated directory as React components
    
    - Imported only one icon for now, others will follow shortlyu

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [
+        react(),
+        svgr({
+            svgrOptions: {
+                // SVGR will optimise the compononents to improve performance.
+                // More here: https://www.vandebron.tech/blog/optimizing-converting-and-exporting-svg-icons-in-react
+                icon: true
+            },
+            // Also include only the dedicated icons directory for obvious performance reasons.
+            include: 'src/assets/icons/*.svg'
+        })
+    ]
 })
